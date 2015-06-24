751331c8152b7bfd296a302e70572dbd29a4c861
Krzysztof Charzynski <krzysztof.charzynski@kcharzynski2-w7.globalintech.pl>
2015-06-24 14:03:45 +0200

    game class implemented

--- game.rb
+++ game.rb
@@ -1,22 +1,40 @@
 class Game
 
-  def initialize(home_team,away_team,score = nil,ispremium = false)
+  def initialize(home_team,away_team,score = nil,ispremium = false, id=0)
     @home_team = home_team
     @away_team = away_team
     @score = score
     @ispremium = ispremium
   end
 
+  def addgametoarray(games)
+    @games = games
+    @games << "#{@home_team} vs. #{@away_team}"
+  end
+
+  def assignid(games)
+    @id = games.index("#{@home_team} vs. #{@away_team}").to_i + 1
+  end
+
+  def hasended?
+    @score ? true : false
+  end
+
   def printgameinfo
-    if @score
-      p "#{@home_team} played with #{@away_team} and the score was #{@score}"
+    if hasended?
+      "#{@home_team} played with #{@away_team} and the score was #{@score}"
     else
-      p "#{@home_team} will play with #{@away_team}"
+      "#{@home_team} will play with #{@away_team}"
     end
   end
 
 end
 
+testgames = Array.new
 testgame = Game.new("Juventus","Barcelona","3:1")
+testgame2 = Game.new("Bayern","Real Madryt")
+testgame.assignid(testgames)
+testgame2.assignid(testgames)
 testgame.printgameinfo
-testgame.home_team
+
+#testgame.home_team
---
+++ game_spec.rb
@@ -0,0 +1,38 @@
+require '.\game.rb'
+
+RSpec.describe Game do
+
+before :each do
+  @testgames = []
+end
+
+before :all do
+  @testgame1 = Game.new("test_home_team_1", "test_away_team_1", "3:1")
+  @testgame2 = Game.new("test_home_team_2", "test_away_team_2")
+end
+
+it 'should properly print game info when game has ended' do
+  expect(@testgame1.printgameinfo).to eq("test_home_team_1 played with test_away_team_1 and the score was 3:1")
+end
+
+it 'should properly print game info when game wasn\'t played yet' do
+  expect(@testgame2.printgameinfo).to eq("test_home_team_2 will play with test_away_team_2")
+end
+
+it 'should properly show game info from games array' do
+  @testgame1.addgametoarray(@testgames)
+  @testgame2.addgametoarray(@testgames)
+  expect(@testgames).to eq(["test_home_team_1 vs. test_away_team_1","test_home_team_2 vs. test_away_team_2"])
+end
+
+it 'should properly assign Id to game' do
+  @testgame1.addgametoarray(@testgames)
+  @testgame2.addgametoarray(@testgames)
+
+  expect(@testgame1.assignid(@testgames)).to eq(1)
+  expect(@testgame2.assignid(@testgames)).to eq(2)
+end
+
+
+
+end
