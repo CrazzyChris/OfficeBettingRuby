d8655ed40c909a4f05124530f79681c483101457
Krzysiek <krzysiek.charzynski@hotmail.com>
2015-07-06 22:27:40 +0200

    user model updated

--- user.rb
+++ user.rb
@@ -1,6 +1,26 @@
 class User
 
-  def initialize
+#attr_reader
+attr_accessor :username, :password, :loggedin
+
+  def initialize(username)
+    @username=username
+    @points = 0
+    @exact_scores = 0
+    @loggedin = false
+    @password = 'Qwerty123'
+  end
+
+  def log_in
+    @loggedin = true
+  end
+
+  def log_out
+    @loggedin = false
+  end
+
+  def change_password(new_password)
+    @password = new_password
   end
 
 end
--- user_spec.rb
+++ user_spec.rb
@@ -3,18 +3,45 @@ require '.\user.rb'
 RSpec.describe User do
 
   before :each do
+    @users = []
+    @user1 = User.new('test_user_1')
+    @user2 = User.new('test_user_2')
   end
 
   describe 'create' do
   end
 
-  describe 'login' do
+  describe 'log_in' do
+
+    it 'should log user in if he is not' do
+      @user1.log_in
+      expect(@user1.loggedin).to eq(true)
+    end
+
+    it 'should inform that user is already logged in' do
+    end
   end
 
-  describe 'logout' do
+  describe 'log_out' do
+
+    it 'should log user out if he is logged in' do
+      @user1.loggedin = true
+      @user1.log_out
+      expect(@user1.loggedin).to eq(false)
+    end
+
+    it 'should inform that user is not already logged on' do
+    end
+
   end
 
-  describe 'changepassword' do
+  describe 'change_password' do
+
+    it 'should change password for the first time' do
+      @user1.change_password('test_pass')
+      expect(@user1.password).to eq('test_pass')
+    end
+
   end
 
   describe 'printuserinfo' do
