e8c89fa5a44877fe4b253d4302ad757d5df067dc
Krzysiek <krzysiek.charzynski@hotmail.com>
2015-07-07 23:03:43 +0200

    created update_score method and tests for it

--- bet.rb
+++ bet.rb
@@ -1,2 +1,20 @@
 class Bet
+
+attr_reader :user, :game
+attr_accessor :home_goals, :away_goals, :points, :exact_score
+
+def initialize(user,game,home_goals,away_goals)
+  @game = game
+  @user = user
+  @home_goals = home_goals
+  @away_goals = away_goals
+  @points = 0
+  @exact_score = false
+end
+
+def update_score(new_home_goals,new_away_goals)
+  @home_goals = new_home_goals
+  @away_goals = new_away_goals
+end
+
 end
--- bet_spec.rb
+++ bet_spec.rb
@@ -1,8 +1,24 @@
 require '.\bet.rb'
 
-Rspec.describe Bet do
+RSpec.describe Bet do
 
   before :each do
+    @bet1 = Bet.new('Juventus vs. FC Barcelona','test_user_1',3,1)
+  end
+
+  describe 'update_score' do
+
+    it 'should update score when changed' do
+      @bet1.update_score(2,1)
+      expect(@bet1.home_goals).to eq(2)
+      expect(@bet1.away_goals).to eq(1)
+    end
+
+    it 'should inform when the score is the same as earlier' do
+    end
+
+    it 'should throw error when any of goal predictions has wrong format' do
+    end
   end
 
 end
