d54f9c639f7854b77d55b6b537ce72b369ad53f0
CrazzyChris <krzysztof.charzynski@ihs.com>
2015-12-17 15:43:01 +0100

    points now are doubled for premium games

--- update_points.rb
+++ update_points.rb
@@ -2,10 +2,11 @@ require 'pg'
 
 conn = PGconn.connect("localhost",5432,"","","test","postgres","phoenix")
 
-res = conn.exec("select bets.id as \"bet id\",users.displayname as name, bets.hometeamgoals as \"predicted home goals\", bets.awayteamgoals as \"predicted away goals\", games.hometeamgoals as \"correct home goals\", games.awayteamgoals as \"correct away goals\" from Bets,Games,Users where bets.gameid = games.id and bets.userid = users.id;")
+res = conn.exec("select bets.id as \"bet id\",users.displayname as name, games.id as \"game id\", bets.hometeamgoals as \"predicted home goals\", bets.awayteamgoals as \"predicted away goals\", games.hometeamgoals as \"correct home goals\", games.awayteamgoals as \"correct away goals\", games.ispremium as \"Premium?\"from Bets,Games,Users where bets.gameid = games.id and bets.userid = users.id;")
 
 res.each do |row|
   bet_id = row['bet id']
+  game_id = row['game id']
   user = row['name']
   predicted_home_goals = row['predicted home goals']
   predicted_away_goals = row['predicted away goals']
@@ -13,11 +14,11 @@ res.each do |row|
   real_away_goals = row['correct away goals']
 
   if predicted_home_goals == real_home_goals and predicted_away_goals == real_away_goals
-    conn.exec("update bets set points = 3 where id = #{bet_id} ")
+    conn.exec("DO $do$ BEGIN IF (SELECT ispremium FROM games where id=#{game_id}) THEN UPDATE bets SET points=6 WHERE gameid=#{game_id} and id=#{bet_id}; ELSE UPDATE bets SET points=3 WHERE gameid=#{game_id} and id=#{bet_id}; END IF; END $do$")
     p "#{user} is awesome"
   elsif (predicted_home_goals > predicted_away_goals and real_home_goals > real_away_goals) or (predicted_home_goals < predicted_away_goals and real_home_goals < real_away_goals)
     p "#{user} is good"
-    conn.exec("update bets set points = 1 where id = #{bet_id} ")
+    conn.exec("DO $do$ BEGIN IF (SELECT ispremium FROM games where id=#{game_id}) THEN UPDATE bets SET points=2 WHERE gameid=#{game_id} and id=#{bet_id}; ELSE UPDATE bets SET points=1 WHERE gameid=#{game_id} and id=#{bet_id}; END IF; END $do$")
   else
     p "#{user} is stupid"
     conn.exec("update bets set points = 0 where id = #{bet_id} ")
